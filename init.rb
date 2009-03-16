23bf86de3cc74b185b1cdf9dcf3838078cae6f21
isaac <isaackearse@gmail.com>
2009-03-16 14:19:18 +1300

    add an email configuration option

--- init.rb
+++ init.rb
@@ -1,1 +1,15 @@
 Liquid::Template.register_tag(:contactform, Mephisto::Liquid::ContactForm)
+
+module ::Mephisto
+  module Plugins
+    class ContactForm < Mephisto::Plugin
+      
+      author 'James Crisp'
+      notes 'Plugin for Mephisto which provides a customisable contact us / feedback form. Feedback / messages are sent via email to an address you specify. Previously hosted at http://mephisto-contact-form-plugin.googlecode.com/svn/trunk/'
+      homepage 'http://jamescrisp.org/2007/03/19/contact-feedback-form-plugin-for-mephisto/'
+      
+      option :email, nil
+      
+    end
+  end
+end
