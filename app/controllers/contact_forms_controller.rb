cd0934f5279579a76ccd26585cfc2ceea1d615b0
isaac <isaackearse@gmail.com>
2009-03-16 15:16:11 +1300

    safely fallback to the email of the sites first user if site email is not set

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,12 +7,15 @@ class ContactFormsController < ApplicationController
   def create
     message = ContactMessage.new(params[:contact])
     if (message.valid?)
-      recipient = Mephisto::Plugin[:contact_form].email || site.email || site.admins.first.email
+      recipient = Mephisto::Plugin[:contact_form].email || site.email.blank? ? site.users.first.email : site.email
       ContactNotifier.deliver_contact_notification(message, recipient)
       render_liquid_template_for(:contact_us, 'message' => "Thank you for your feedback.", :status => "200 OK")
     else
       render_liquid_template_for(:contact_us, 'errors' => message.errors.full_messages, 
         'submitted' => params[:contact], :status => "200 OK")
     end
+  rescue
+    debugger
+    self
   end
 end
