439cf38ff7d5da15ffa2d3aa2bfb49f916859088
isaac <isaackearse@gmail.com>
2009-03-16 14:49:10 +1300

    get recipient email from plugin config or sites email

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,7 +7,8 @@ class ContactFormsController < ApplicationController
   def create
     message = ContactMessage.new(params[:contact])
     if (message.valid?)
-      ContactNotifier.deliver_contact_notification(message)
+      recipient = Mephisto::Plugin[:contact_form].email || site.email
+      ContactNotifier.deliver_contact_notification(message, recipient)
       render_liquid_template_for(:contact_us, 'message' => "Thank you for your feedback.", :status => "200 OK")
     else
       render_liquid_template_for(:contact_us, 'errors' => message.errors.full_messages, 
--- app/models/contact_notifier.rb
+++ app/models/contact_notifier.rb
@@ -3,8 +3,8 @@ class ContactNotifier < ActionMailer::Base
   
   self.template_root = File.dirname(__FILE__) + '/views'
   
-  def contact_notification(contact_message)
-    recipients      "ENTER DESTINATION EMAIL ADDRESS HERE"
+  def contact_notification(contact_message, recipient)
+    recipients      recipient
     from            contact_message.author_email
     subject         "[Mephisto Contact Form] #{contact_message.subject}"
     body            "contact_message" => contact_message
