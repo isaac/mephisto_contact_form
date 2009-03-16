6a7fe637ef56a5f2879b32ef1adb095070423cd0
isaac <isaackearse@gmail.com>
2009-03-16 14:52:58 +1300

    email defaults to the sites first admin if not set for the site

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,7 +7,7 @@ class ContactFormsController < ApplicationController
   def create
     message = ContactMessage.new(params[:contact])
     if (message.valid?)
-      recipient = Mephisto::Plugin[:contact_form].email || site.email
+      recipient = Mephisto::Plugin[:contact_form].email || site.email || site.admins.first.email
       ContactNotifier.deliver_contact_notification(message, recipient)
       render_liquid_template_for(:contact_us, 'message' => "Thank you for your feedback.", :status => "200 OK")
     else
